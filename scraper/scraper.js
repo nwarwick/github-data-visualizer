cac5b512666d922e31e4ea61e8f81a4eece9d130
Rodney Gelera <rodneyg94@gmail.com>
2017-03-20 14:10:18 -0700

    output data to json file in geojson format

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -12,21 +12,20 @@ var geocoder = NodeGeocoder(options);
 
 var inputFile = 'data/users.csv';
 var outputFile = 'scraped_data/data.json';
-var users = [];
+var users = {"type": "FeatureCollection",
+			 "features": []};
 
 // get list of users from csv file
 fs.createReadStream(inputFile)
 	.pipe(parse({delimiter: ','}))
 	.on('data', function(csvrow) {
+		var limit = 5;
 		// process each user asyncronously
-		async.eachSeries(csvrow, function userProfile(user, callback) {
-			console.log("Processing user: " + user + "...");
-			url = user;
-			//if (url == "errfree") {
-				//console.log("WTF IS GOING ON");
-			//	callback();
-				// don't have time to make a better workaround
-			//} else {
+		async.eachSeries(
+			csvrow.slice(0,limit - 1),
+			function(user, callback) {
+				console.log("Processing user: " + user + "...");
+				url = user;
 				gs(url, function(err, data) {
 					//console.log(user + " profile data: \n" + JSON.stringify(data));
 					if (typeof data == "object") {
@@ -44,20 +43,23 @@ fs.createReadStream(inputFile)
 								geojson = {};
 								geojson.type = "Feature";
 								geojson.geometry = {"type": "Point", "coordinates": [res[0].latitude, res[0].longitude]};
-								//users.push(data);
+								geojson.properties = {"user": user, "language": ""};
+								users.features.push(geojson);
 								//console.log(data);
 								callback();
 							});
-							callback();
 						} else { //empty location
 							console.log("--- No location, skipping.");
 							callback();
 						}	
-					}
-					
+					}		
 				});
-			//}
-		});
+			},
+			function done(){
+				// called when everything is done
+				jsonfile.writeFile(outputFile, users);
+			}
+		);
 	});
 
 
