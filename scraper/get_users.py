a6f32d5d35ee03c4b66996a536be3c521181409f
Rodney Gelera <rodneyg94@gmail.com>
2017-03-19 18:56:59 -0700

    python script for getting list of users; start python script for scraping user data based on list of users

---
+++ scraper/get_users.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+# get list of users
+users = []
+
+r = int(input('Number of requests to make: '))
+#r = 20 # number of requests to make
+
+user_start = int(input('User ID to start at (inclusive): '))
+since = user_start - 1
+for i in range(0, r):
+	r = requests.get('https://api.github.com/users?since={}'.format(since))
+	if r.status_code is not 200:
+		print r.json()['message']
+		break
+	j = r.json()
+	for u in j:
+		users.append(u['login'])
+	end_user_id = u['id']
+	filename = './data/users{:0>5}-{:0>5}.txt'.format(since + 1, end_user_id)
+	with open(filename, 'w') as outfile:
+		json.dump(j, outfile)
+	since = end_user_id
+
+print "Number of users found: " + str(len(users))
+users_file = open('./data/users.csv', 'a')
+for u in users:
+	users_file.write("{},".format(u))
