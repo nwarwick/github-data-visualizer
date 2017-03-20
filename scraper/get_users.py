4ebde58fd2b3643172c0c6952d513a338cc06d2b
Rodney Gelera <rodneyg94@gmail.com>
2017-03-19 23:58:25 -0700

    fixed csv file, leftover merge conflict line

--- scraper/get_users.py
+++ scraper/get_users.py
@@ -11,7 +11,11 @@ print "and PULL master before running this!"
 r = int(input('Number of requests to make (60 max per IP per hour): '))
 #r = 20 # number of requests to make
 
+print ""
+print "Make sure to pick a range of user IDs that have not been looked up yet. Check text files in data too see which ones are done. 30 users are looked up for every request."
+print "Ex. user_start = 1000, requests = 60 (1800 users). IDs found = 1000 - ~2799 (some ids may be missing)"
 user_start = int(input('User ID to start at (inclusive): '))
+end_user_id = user_start
 since = user_start - 1
 for i in range(0, r):
 	r = requests.get('https://api.github.com/users?since={}'.format(since))
@@ -29,6 +33,7 @@ for i in range(0, r):
 		json.dump(j, outfile)
 	since = end_user_id
 
+print "Checked user ids: " + str(user_start) + " - " + str(end_user_id)
 print "Number of users found: " + str(len(users))
 users_file = open('./data/users.csv', 'a')
 for u in users:
