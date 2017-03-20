5ea4aa15b2f0b2a16a6b83c9a48c4959f0fb180e
Rodney Gelera <rodneyg94@gmail.com>
2017-03-20 12:06:14 -0700

    scraper asyncronously processes user profiles

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,27 +1,42 @@
 var gs = require('github-scraper');
 var fs = require('fs');
 var parse = require('csv-parse');
-//var async = require('async');
+var async = require('async');
+var jsonfile = require('jsonfile');
+
+var inputFile = 'data/users.csv';
+var outputFile = 'scraped_data/data.json'
+var data = {}
+users = []
 
 // get list of users from csv file
-var inputFile = 'data/users.csv';
-var csvData = [];
 fs.createReadStream(inputFile)
 	.pipe(parse({delimiter: ','}))
 	.on('data', function(csvrow) {
-		//for (var i = 0; i < csvrow.length - 1; i++) {
-		for (var i = 0; i < 10; i++) {
-			userProfile(csvrow[i]);
-		}
+		// process each user asyncronously
+		async.eachSeries(csvrow, function userProfile(user, callback) {
+			console.log("Processing user: " + user + "...");
+			url = user;
+			if (url === "errfree") {
+				callback();
+				// don't have time to make a betterworkaround
+			}
+			gs(url, function(err, data) {
+				//console.log(user + " profile data: \n" + data);
+				data = JSON.parse(data);
+				if (data.location) {
+					users.push(data);
+					console.log(data);
+					callback();
+				} else { //empty location
+					console.log("--- No location, skipping.");
+					callback();
+				}	
+			});
+		});
 	});
 
-function userProfile(user) {
-	console.log("Processing user: " + user + "...");
-	url = user;
-	gs(url, function(err, data) {
-		console.log(user + " profile data: \n" + data);
-	});
-}
+
 /*
 var url = val; // github username goes here
 */
